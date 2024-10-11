6ac9b273dc5a989f94c8f2f737819cdd79712b1f
firefighter69 <grishabratmashi@gmail.com>
2024-10-11 17:44:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#1
+my_string = input()
+def findLen(my_string):
+    counter = 0
+    for i in my_string:
+        counter += 1
+    return counter
+print(findLen(my_string))
+
+#2
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+
+#3
+my_string
+print(my_string[0])
+print(my_string[-1])
+#############
+
