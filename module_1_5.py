3d4d3c068f93a7973f98efcf8dec73e754323faa
firefighter69 <grishabratmashi@gmail.com>
2024-10-13 21:24:49 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple_ = ([1,3,5,7],"homework")
+print(tuple_)
+tuple_[0][3] = 3
+print(tuple_)
+mutable_list = ([1,8,7,9,5,3],"list")
+print(mutable_list)
+mutable_list[0][3] = 6
+mutable_list[0][2] = 4
+print(mutable_list)
